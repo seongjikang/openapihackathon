e16613b2ff24fcc25109bca1719e3af65b812b85
kspook <sangdonglee@hotmail.com>
2019-11-24 07:50:56 +0900

    phone number get
    phone number get

--- app/src/main/java/com/shinhan/dos/bonus/SplashActivity.java
+++ app/src/main/java/com/shinhan/dos/bonus/SplashActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -21,74 +24,144 @@ import java.util.Map;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static android.Manifest.permission.READ_PHONE_NUMBERS;
+import static android.Manifest.permission.READ_PHONE_STATE;
+import static android.Manifest.permission.READ_SMS;
+
 public class SplashActivity extends AppCompatActivity {
 
-	private static final String TAG = "SplashActivity";
+    private static final String TAG = "SplashActivity";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
+    private SharedPreferences pref;
 
-		launchApp();
-	}
+    private String mHpno;
+    private String mName;
 
-	private void launchApp() {
-		checkPermissions();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-		SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
-		Boolean initUser = pref.getBoolean("initUser", true);
+        launchApp();
+    }
 
-		if (initUser) {
-			setMySalaryInfo();
-		} else {
-			getFinancialInfo();
-		}
-	}
+    private void launchApp() {
+        checkPermissions();
 
-	// TODO : 권한체크
-	private void checkPermissions() {
-//		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//		builder.setMessage(R.string.set_permission);
-	}
 
-	private void setMySalaryInfo() {
-		startActivity(new Intent(this, UserInfoActivity.class));
-		finish();
-	}
+    }
 
-	// 사용자정보 불러오기
-	private void getFinancialInfo() {
-		Map params = new LinkedHashMap<>();
-		params.put("hpno", "01071444074");
-		DataResult dataResult = new DataResultImpl();
-		dataResult.getName(new Callback<JsonObject>() {
-			@Override
-			public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+    // TODO : 권한체크
+    private void checkPermissions() {
+        pref = getSharedPreferences("pref", MODE_PRIVATE);
+        mHpno = pref.getString("hpno", "");
+        mName = pref.getString("name", "");
+
+        if (ActivityCompat.checkSelfPermission(this, READ_SMS) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, READ_PHONE_NUMBERS) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            TelephonyManager tMgr = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+            String mPhoneNumber = tMgr.getLine1Number();
+            Log.d(TAG, "1 hpno = " + mPhoneNumber);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString("hpno", mPhoneNumber);
+            editor.commit();
+            mHpno = mPhoneNumber;
+            return;
+        } else {
+            requestPermission();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 100: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0) {
+
+                    SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+                    Boolean initUser = pref.getBoolean("initUser", true);
+
+                    TelephonyManager tMgr = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+                    if (checkSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    Activity#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for Activity#requestPermissions for more details.
+                        return;
+                    }
+                    String mPhoneNumber = tMgr.getLine1Number();
+                    Log.d(TAG, "1 hpno = " + mPhoneNumber);
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putString("hpno", mPhoneNumber);
+                    editor.commit();
+                    mHpno = mPhoneNumber;
+
+                    if (initUser) {
+                        setMySalaryInfo();
+                    } else {
+                        getFinancialInfo();
+                    }
+
+                } else {
+
+
+                }
+                return;
+            }
+
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{READ_SMS, READ_PHONE_NUMBERS, READ_PHONE_STATE}, 100);
+        }
+    }
+
+    private void setMySalaryInfo() {
+        startActivity(new Intent(this, UserInfoActivity.class));
+        finish();
+    }
+
+    // 사용자정보 불러오기
+    private void getFinancialInfo() {
+        Map params = new LinkedHashMap<>();
+        params.put("hpno", "01071444074");
+        DataResult dataResult = new DataResultImpl();
+        dataResult.getName(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
 //				response.body.get("dataBody").getAsJsonObject().get("insuranceList").getAsJsonArray().get(0)
-				Log.d(TAG, response.toString());
+                Log.d(TAG, response.toString());
 
-				goToMainActivity();
-			}
+                goToMainActivity();
+            }
 
-			@Override
-			public void onFailure(Call<JsonObject> call, Throwable t) {
-				Log.d(TAG, "FAIL....");
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Log.d(TAG, "FAIL....");
 
-				goToMainActivity();
-			}
-		}, params);
-	}
+                goToMainActivity();
+            }
+        }, params);
+    }
 
 
-	private void goToMainActivity() {
-		Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-		startActivity(intent);
-		finish();
-	}
+    private void goToMainActivity() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
 }
